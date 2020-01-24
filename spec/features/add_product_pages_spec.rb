3d1403800b24070996122659d1fd862978fa78ca
Angela Weber <angelaweber95@hotmail.com>
2020-01-24 15:06:50 -0800

    attempt to debug integration test

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -1,24 +1,24 @@
-describe "the add a product process" do
+require 'rails_helper'
+
+
+describe('create an product path', {:type => :feature})  do
   before(:each) do
+    visit('/users/sign_up')
     fill_in 'Email', :with => 'ela@v.com'
     fill_in 'Password', :with => 'cats123'
     fill_in 'Password confirmation', :with => 'cats123'
+    choose('Yes')
     click_on 'Sign Up'
-    click_on 'Sign out'
-    click_on 'Sign-in'
-    fill_in 'Email', :with => 'ela@v.com'
-    fill_in 'Password', :with => 'cats'
-    click_on 'Sign in'
   end
   it "adds a new product" do
-  visit products_path
-  click_link 'Create new product'
-  fill_in 'Name', :with => 'Chair'
-  fill_in 'Cost', :with => '12'
-  fill_in 'Country of origin', :with => 'United States'
-  click_on 'Create Product'
-  expect(page).to have_content 'Product successfully added!'
-  expect(page).to have_content 'Chair'
-end
+    visit products_path
+    click_link 'Create new product'
+    fill_in 'Name', :with => 'Chair'
+    fill_in 'Cost', :with => '12'
+    fill_in 'Country of origin', :with => 'United States'
+    click_on 'Create Product'
+    expect(page).to have_content 'Product successfully added!'
+    expect(page).to have_content 'Chair'
+  end
 
 end
